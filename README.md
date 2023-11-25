e6e44a798c1bad21d54b22a521d94738517faebb
Egor Ternovoy <cofob@riseup.net>
2023-11-26 00:01:16 +0700

    v0

---
+++ README.md
@@ -0,0 +1,14 @@
+# Gosuslugi
+
+Unified portal of state and municipal services right in your favorite IDE.
+
+## Features
+
+- [x] Your favorite government services portal right in your IDE! You don't need a browser anymore!
+      All programming questions can be asked to robot Max!
+- [x] A website traffic counter that sends information about you to a government server every 10 seconds.
+      Now IDE complies with the data collection law too!
+
+## Usage
+
+Press `F1` and enter `Gosuslugi` and just hit `Enter`!
---
+++ src/extension.ts
@@ -0,0 +1,52 @@
+import * as vscode from "vscode";
+import axios from "axios";
+
+const html = `<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<style>
+			iframe {
+				height: 1000px;
+				width: 100%;
+				resize: both;
+				overflow: auto;
+			}
+		</style>
+    </head>
+    <body>
+		<iframe src="https://gosuslugi.ru/"></iframe>
+    </body>
+</html>`;
+
+function ping() {
+  // I can't find info about gos metrics API, so I just make requests to some government sites
+  axios.post("https://ya.ru/vscode-gosuslugi");
+  axios.post("https://kremlin.ru/vscode-gosuslugi");
+  axios.post("https://vk.ru/vscode-gosuslugi");
+  axios.post("https://ok.ru/vscode-gosuslugi");
+}
+
+export function activate(context: vscode.ExtensionContext) {
+  let disposable = vscode.commands.registerCommand("gosuslugi.open", () => {
+    const panel = vscode.window.createWebviewPanel(
+      "gosuslugi.web",
+      "Unified portal of state and municipal services",
+      {
+        viewColumn: vscode.ViewColumn.Beside,
+        preserveFocus: true,
+      },
+      { enableScripts: true }
+    );
+
+    panel.reveal();
+    panel.webview.html = html;
+  });
+
+  context.subscriptions.push(disposable);
+
+  // ping every 10 seconds
+  setInterval(ping, 10000);
+}
+
+export function deactivate() {}
